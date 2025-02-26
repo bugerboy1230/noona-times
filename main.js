4651d5848f84aeed1c9256209fc046f457b2e81f
ChunWon Yoon <bugerboy1230@naver.com>
2025-02-26 22:45:51 +0900

    API URL Changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY='d9e72cb017b448d59b9db8c85da9662c';
 let news = [];
 const getLatestNews = async () => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=science`
     );
     const response = await fetch(url);
     const data = await response.json();
